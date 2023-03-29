05ea7aa90365b50d6a96c815268e15ae9265e798
Eric Ryu <ericryu19@gmail.com>
2023-03-28 20:02:48 -0400

    use example component, page component, and finish routing

--- easy_chef/src/App.js
+++ easy_chef/src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import React from "react";
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
-
+import ExampleComponent from "./components/ExampleComponent/ExampleComponent";
+import LoginPage from "./pages/LoginPage/LoginPage";
 
 function App() {
 
-  function DefaultAppComponent() {
+  function HomePageComponent() {
     return <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -22,13 +23,22 @@ function App() {
           Learn React
         </a>
       </header>
+      <ExampleComponent text="This is an example component in Div" />
     </div>
   }
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={'/'} element={<DefaultAppComponent />} />
+        <Route path="/" element={<HomePageComponent />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<HomePageComponent />} />
+        <Route path="/accounts/edit-profile" element={<HomePageComponent />} />
+        <Route path="/accounts/my-recipe" element={<HomePageComponent />} />
+        <Route path="/recipes/create-recipe" element={<HomePageComponent />} />
+        <Route path="/recipes/edit-recipe/:recipeId" element={<HomePageComponent />} />
+        <Route path="/recipes/recipe-details/:recipeId" element={<HomePageComponent />} />
+        <Route path="/shopping-cart" element={<HomePageComponent />} />
       </Routes>
     </BrowserRouter>
   );
