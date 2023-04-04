6d06702af130e7d80a860be946e62e2914c6f30a
Eric Ryu <ericryu19@gmail.com>
2023-04-04 15:02:33 -0400

    set recipe context and backend handling

--- easy_chef/src/pages/CreateRecipe/CreateRecipe.jsx
+++ easy_chef/src/pages/CreateRecipe/CreateRecipe.jsx
@@ -1,5 +1,37 @@
-import react from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useRecipeContext } from '../../contexts/RecipeContext/RecipeContext';
+import { useState } from 'react';
+import fetchBackend from '../../Utils/fetchBackend';
+import { Grid } from '@mui/material';
+import Navbar from '../../components/Navbar/Navbar';
 
-function CreateRecipe() {}
+function CreateRecipe() {
+    const navigate = useNavigate();
+    const { setRecipeId } = useRecipeContext();
+    const [formError, setFormError] = useState({
+        errorOccurred: false,
+        errorMsg: ''
+    });
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        // get values for recipe creation
+        const data = new FormData(event.currentTarget);
+        const dataToSend = {};
+
+        fetchBackend
+            .post('/recipes/create-recipe', dataToSend)
+            .then((response) => {})
+            .catch((error) => {});
+    };
+    return (
+        <Grid container spacing={2} sx={{ textAlign: 'center' }}>
+            <Grid item xs={12}>
+                <Navbar></Navbar>
+            </Grid>
+        </Grid>
+    );
+}
 
 export default CreateRecipe;
