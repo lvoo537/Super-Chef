524bc1ceaa46858716749604cd8e088fba08ac17
Eric Ryu <ericryu19@gmail.com>
2023-04-10 23:10:30 -0400

    re-center ui and add recipe images carousel

--- easy_chef/src/pages/CreateRecipe/CreateRecipe.jsx
+++ easy_chef/src/pages/CreateRecipe/CreateRecipe.jsx
@@ -3,12 +3,13 @@ import { useRecipeContext } from '../../contexts/RecipeContext/RecipeContext';
 import { useState } from 'react';
 import fetchBackend from '../../Utils/fetchBackend';
 import encodeImages from '../../Utils/encodeImages';
-import { Grid, TextField, Box } from '@mui/material';
+import { Grid, TextField, Box, Typography } from '@mui/material';
 import Navbar from '../../components/Navbar/Navbar';
 import IngredientsTable from '../../components/IngredientsTable/IngredientsTable';
 import AddInstructionsComponent from '../../components/AddInstructionsComponent/AddInstructionsComponent';
 import Button from '@mui/material/Button';
 import { CreateRecipeIngredientsContext } from '../../contexts/CreateRecipeIngredientsContext/CreateRecipeIngredientsContext';
+import Carousel from '../../components/Carousel/Carousel';
 
 function CreateRecipe() {
     const navigate = useNavigate();
@@ -47,19 +48,18 @@ function CreateRecipe() {
                 <Box component="form" onSubmit={handleSubmit}>
                     <Grid
                         container
-                        spacing={1}
+                        spacing={2}
                         margin="auto"
-                        direction="column"
                         alignItems="center"
                         justifyContent="center"
                     >
-                        <Grid item xs={4}>
+                        <Grid item xs={1}>
                             <TextField id="recipe-name" label="Recipe Name" variant="outlined" />
                         </Grid>
-                        <Grid item xs={4}>
+                        <Grid item xs={1}>
                             <TextField id="cooking-time" label="Cooking Time" variant="outlined" />
                         </Grid>
-                        <Grid item xs={4} marginTop={1} marginLeft={23.8}>
+                        <Grid item xs={1} marginLeft={0} marginTop={3.8}>
                             <Button variant="contained" component="label">
                                 Upload Recipe Images
                                 <input
@@ -78,15 +78,37 @@ function CreateRecipe() {
                                 disabled
                             />
                         </Grid>
-                        <Grid item xs={12} marginLeft={2}>
-                            <CreateRecipeIngredientsContext.Provider
-                                value={{ ingredients, setIngredients }}
+                        {imagesEncoded.length === 0 ? (
+                            <div></div>
+                        ) : (
+                            <Grid item xs={12} display="flex" justifyContent="center">
+                                <Carousel images={imagesEncoded} />
+                            </Grid>
+                        )}
+                        <Grid item xs={12}>
+                            <Grid
+                                container
+                                direction="column"
+                                alignItems="center"
+                                justifyContent="center"
                             >
-                                <IngredientsTable width={750} />
-                            </CreateRecipeIngredientsContext.Provider>
-                        </Grid>
-                        <Grid item xs={12} marginLeft={2}>
-                            <AddInstructionsComponent />
+                                <Grid item xs={12}>
+                                    <Typography variant="h5" sx={{ mb: 1 }}>
+                                        Recipe Ingredients
+                                    </Typography>
+                                    <CreateRecipeIngredientsContext.Provider
+                                        value={{ ingredients, setIngredients }}
+                                    >
+                                        <IngredientsTable width={750} />
+                                    </CreateRecipeIngredientsContext.Provider>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Typography variant="h5" sx={{ mb: 2, mt: 4 }}>
+                                        Recipe Instructions
+                                    </Typography>
+                                    <AddInstructionsComponent />
+                                </Grid>
+                            </Grid>
                         </Grid>
                     </Grid>
                 </Box>
