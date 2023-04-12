40f838a0f341304bd086a5519fd106b9a8b8b8f5
Eric Ryu <ericryu19@gmail.com>
2023-04-12 18:35:34 -0400

    create AuthProvider wrapper

--- easy_chef/src/contexts/Auth/AuthContext.jsx
+++ easy_chef/src/contexts/Auth/AuthContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useState } from 'react';
+import fetchBackend from '../../Utils/fetchBackend';
 
 export const AuthContext = createContext({
     authenticated: false,
@@ -6,6 +7,29 @@ export const AuthContext = createContext({
     uid: '',
     setUid: (uid) => {}
 });
+
+export const AuthProvider = ({ children }) => {
+    const [authenticated, setAuthenticated] = useState(false);
+    const [uid, setUid] = useState('');
+
+    fetchBackend.interceptors.request.use(
+        (config) => {
+            const token = localStorage.getItem('access');
+            if (token) config.headers.Authorization = `Bearer ${token}`;
+            return config;
+        },
+        (error) => Promise.reject(error)
+    );
+
+    const value = {
+        authenticated,
+        setAuthenticated,
+        uid,
+        setUid
+    };
+
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
 
 export function useAuthContext() {
     return useContext(AuthContext);
