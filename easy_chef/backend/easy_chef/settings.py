aee70a0e3ecdf54ffa699692a790fbf99b6afb2d
Eric Ryu <ericryu19@gmail.com>
2023-04-12 18:53:46 -0400

    install corsheaders middleware and configure settings

--- easy_chef/backend/easy_chef/settings.py
+++ easy_chef/backend/easy_chef/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'accounts',
     'recipes',
     'search',
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +53,13 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
+
+# Allow CORS from everywhere
+# CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'easy_chef.urls'
 
