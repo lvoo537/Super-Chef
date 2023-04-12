1a6aac095fd26511aa34e48b1c47de215bb546d4
Eric Ryu <ericryu19@gmail.com>
2023-04-12 18:42:41 -0400

    add jwt header config in fetchBackend

--- easy_chef/src/contexts/Auth/AuthContext.jsx
+++ easy_chef/src/contexts/Auth/AuthContext.jsx
@@ -8,29 +8,6 @@ export const AuthContext = createContext({
     setUid: (uid) => {}
 });
 
-export const AuthProvider = ({ children }) => {
-    const [authenticated, setAuthenticated] = useState(false);
-    const [uid, setUid] = useState('');
-
-    fetchBackend.interceptors.request.use(
-        (config) => {
-            const token = localStorage.getItem('access');
-            if (token) config.headers.Authorization = `Bearer ${token}`;
-            return config;
-        },
-        (error) => Promise.reject(error)
-    );
-
-    const value = {
-        authenticated,
-        setAuthenticated,
-        uid,
-        setUid
-    };
-
-    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
-};
-
 export function useAuthContext() {
     return useContext(AuthContext);
 }
