d6e47d380a0e712a89edaa45aa1f3cb26d885b1a
Eric Ryu <ericryu19@gmail.com>
2023-04-15 21:55:37 -0400

    run django in bg and start frontend server

--- easy_chef/run.sh
+++ easy_chef/run.sh
@@ -4,5 +4,8 @@ echo "Activating virtual environment..."
 source venv/bin/activate
 
 echo "Starting Django server..."
-python manage.py runserver
+python manage.py runserver &
+
+echo "Starting the frontend..."
+npm run start
 
